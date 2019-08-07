c5803ef2c25acd7042841f18120df120cb8ab736
Efim Shliamin <shliamin@icloud.com>
2019-08-07 22:13:36 +0300

    creating a search function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+const results = document.querySelector("#results");
 
+
+const searchMovies = (keyword) => {
+  fetch(`http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        const movie = `<li class="list-inline-item">
+          <img src="${result.Poster}" alt="">
+          <p>"${result.Title}"</p>
+          <p>Год: ${result.Year}</p>
+        </li>`;
+        results.insertAdjacentHTML("beforeend", movie);
+      });
+    });
+};
+
+const searchForm = document.querySelector("#search-movies");
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  results.innerHTML = "";
+  const keyword = document.querySelector("#keyword").value;
+  searchMovies(keyword);
+});
